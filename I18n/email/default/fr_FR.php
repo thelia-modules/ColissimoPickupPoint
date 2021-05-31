cadcc46204898b6e748fd12d8d58484f98001e22
jcournut <jcournut@openstudio.fr>
2021-05-31 12:12:53 +0200

    Change order confirmation email

---
+++ I18n/email/default/fr_FR.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    ' and allows you to follow your package online at <a href="https://www.colissimo.fr/portail_colissimo/suivreResultat.do?parcelnumber=%package">https://www.colissimo.fr/portail_colissimo/suivreResultat</a>' => '. Il vous permet de suivre votre colis en ligne sur le site <a href="https://www.colissimo.fr/portail_colissimo/suivreResultat.do?parcelnumber=%package">https://www.colissimo.fr/portail_colissimo/suivreResultat</a>',
+    'Hello' => 'Bonjour',
+    'Please display this message in HTML' => 'Afficher ce message en HTML',
+    'Thank you for your shopping with us and hope to see you soon' => 'Nous vous remercions pour votre achat et espérons vous revoir très vite',
+    'We are pleased to inform you that your order number' => 'Nous sommes heureux de vous informer que votre commande N°',
+    'Your order has been taking care of by La Poste.' => 'Votre commande a été prise en charge par La Poste.',
+    'Your order confirmation Nº %ref' => 'Votre commande N° %ref',
+    'Your shop ' => 'L\'équipe ',
+    'has been shipped on' => 'a été envoyé avec succès le',
+    'Your tracking number is ' => 'Votre numéro de suivi est ',
+    'See you soon,' => 'À très vite,',
+    ' on ' => ' sur ',
+    'Upon receipt, you can withdraw your order in your pickup point:' => 'À réception, vous pourrez retirer votre commande dans votre point relais :'
+);
